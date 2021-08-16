d6e4ffd50f7b332b8e64743b99e409a72ff7c0e7
lclass <88320133+lclass@users.noreply.github.com>
2021-08-16 13:26:33 +0200

    parsing the input and creating a dictionary with names as keys and 0 as value. the next step is to create a function that interprets the scores and subsequently updates the dictionary

--- main.py
+++ main.py
@@ -17,17 +17,27 @@ string = """Anna
     Charlotte Anna 1 0
     Bob Femke 1 0"""
 
+list = []
+
 
 def splitting_input(some_string):
     """splitting the input string on every empty tab"""
     n = 0
-    list = []
     a_str = some_string.split('\n\n')
-    for new_string in a_str:
+    for _ in a_str:
         new_string = a_str[n].split()
         list.append(new_string)
         n += 1
     return list
 
 
-print(splitting_input(string))
+splitting_input(string)
+name = list[0]
+
+
+def creating_dictionary(some_list):
+    a_dict = dict.fromkeys(some_list, 0)
+    print(a_dict)
+
+
+creating_dictionary(name)
